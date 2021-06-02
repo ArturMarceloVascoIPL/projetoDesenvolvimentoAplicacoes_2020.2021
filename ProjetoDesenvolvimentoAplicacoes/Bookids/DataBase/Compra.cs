7b5d0c25533a95a0d5e322fe1c0f5cba22f3b643
Marcelo Marques <2200428@my.ipleiria.pt>
2021-06-02 11:31:34 +0100

    Auto stash before checking out "origin/master"

---
+++ ProjetoDesenvolvimentoAplicacoes/Bookids/DataBase/Compra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookids
+{
+    partial class Compra
+    {
+        public Compra(string Data, bool UtilizouCartao, int IdCliente)
+        {
+            this.Data = Data;
+            this.UtilizouCartao = UtilizouCartao;
+            this.IdCliente = IdCliente;
+        }
+    }
+}
---
+++ ProjetoDesenvolvimentoAplicacoes/Bookids/Repositorios/RepositorioCompras.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookids
+{
+    class RepositorioCompras
+    {
+        Model1Container model = new Model1Container();
+
+        public RepositorioCompras()
+        {
+
+        }
+        public List<Compras> GetCompras()
+        {
+            return model.Compras.Find();
+        }
+    }
+}
