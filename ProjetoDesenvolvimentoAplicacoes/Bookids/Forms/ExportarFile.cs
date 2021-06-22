157206fa5247f8e513f96b06e02cd9b996be3c8b
Artur Quaresma <artursquaresma@gmail.com>
2021-06-22 20:55:03 +0100

    Update ExportarFile.cs

--- ProjetoDesenvolvimentoAplicacoes/Bookids/Forms/ExportarFile.cs
+++ ProjetoDesenvolvimentoAplicacoes/Bookids/Forms/ExportarFile.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,150 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace Bookids.Forms
 {
     public partial class ExportarFile : Form
     {
-        bool fichaInsc = false;
-        bool listaInsc = false;
-        bool faturaVenda = false;
-
-        Evento evento;
-        List<Pessoa> listaParticipantes;
-
         public ExportarFile()
         {
-
-        }
-
-        public ExportarFile(Evento evento)
-        {
-            fichaInsc = true;
-            listaInsc = false;
-            faturaVenda = false;
-
-            this.evento = evento;
-
             InitializeComponent();
         }
 
-        public ExportarFile(List<Pessoa> listaParticipantes)
-        {
-            fichaInsc = false;
-            listaInsc = true;
-            faturaVenda = false;
-
-            this.listaParticipantes = listaParticipantes;
-
-            InitializeComponent();
-        }
-
+        public string option { get; set; }
 
         private void buttonClose_Click(object sender, EventArgs e)
         {
             Close();
         }
 
-        #region ButtonMisc
-
-        private void buttonPdf_MouseEnter(object sender, EventArgs e)
+        private void buttonPDF_MouseHover(object sender, EventArgs e)
         {
-            buttonPdf.Font = new Font(buttonPdf.Font, FontStyle.Bold);
-            buttonPdf.ForeColor = Color.Green;
+            buttonPDF.BackColor = Color.Red;
+            buttonTXT.BackColor = Color.White;
         }
 
-        private void buttonPdf_MouseLeave(object sender, EventArgs e)
+        private void buttonPDF_MouseLeave(object sender, EventArgs e)
         {
-            buttonPdf.Font = new Font(buttonPdf.Font, FontStyle.Regular);
-            buttonPdf.ForeColor = Color.Black;
+            buttonPDF.BackColor = Color.White;
+            buttonTXT.BackColor = Color.Red;
         }
 
-        private void buttonTxt_MouseEnter(object sender, EventArgs e)
+        private void buttonTXT_MouseHover(object sender, EventArgs e)
         {
-            buttonTxt.Font = new Font(buttonTxt.Font, FontStyle.Bold);
-            buttonTxt.ForeColor = Color.Green;
+            buttonPDF.BackColor = Color.White;
+            buttonTXT.BackColor = Color.Red;
         }
 
-        private void buttonTxt_MouseLeave(object sender, EventArgs e)
+        private void buttonTXT_MouseLeave(object sender, EventArgs e)
         {
-            buttonTxt.Font = new Font(buttonTxt.Font, FontStyle.Regular);
-            buttonTxt.ForeColor = Color.Black;
+            buttonPDF.BackColor = Color.White;
+            buttonTXT.BackColor = Color.White;
         }
 
-        #endregion
-
-        private void buttonTxt_Click(object sender, EventArgs e)
+        private void buttonPDF_Click(object sender, EventArgs e)
         {
-            saveFileDialogExport.Filter = "Text Files | *.txt";
-            saveFileDialogExport.DefaultExt = "txt";
-
-            saveFileDialogExport.ShowDialog();
-            string filePath = saveFileDialogExport.FileName;
-
-            if (fichaInsc == true)
-            {
-                try
-                {
-                    string[] fileContent = new string[] { "Ficha de Incrição\n", $"Evento: {evento.Descricao}", $"Local: {evento.Local}", $"Tipo de Evento: {evento.TipoEvento}", "\nNome do Parente: __________\nNome do Filho: __________\n\nParticipa?\nSim __ Nao __" };
-
-                    using (StreamWriter outputfile = new StreamWriter(filePath))
-                    {
-                        foreach (var fileLine in fileContent)
-                        {
-                            outputfile.WriteLine(fileLine);
-                        }
-                    }
-
-                    MessageBox.Show($"Guardado com Sucesso em\n{filePath}", "Ficheiro Guardado");
-                }
-                catch (Exception err)
-                {
-                    MessageBox.Show($"Ocorreu um erro ao guardar o ficheiro!\n{err.Message}", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            else
-            {
-                if (listaInsc == true)
-                {
-                    try
-                    {
-                        using (StreamWriter outputfile = new StreamWriter(filePath))
-                        {
-                            outputfile.WriteLine("Lista de Participantes:\n");
-
-                            foreach (var item in listaParticipantes)
-                            {
-                                outputfile.WriteLine(item);
-                            }
-                        }
-
-                        MessageBox.Show($"Guardado com Sucesso em\n{filePath}", "Ficheiro Guardado");
-                    }
-                    catch (Exception err)
-                    {
-                        MessageBox.Show($"Ocorreu um erro ao guardar o ficheiro!\n{err.Message}", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                else
-                {
-                    if (faturaVenda == true)
-                    {
-
-                    }
-                    else
-                    {
-                        // Debug
-                        MessageBox.Show("Erro. Todos os bools a 'false'");
-                    }
-                }
-            }
+            option = "PDF";
+            DialogResult = DialogResult.OK;
+            Close();
         }
 
-        private void buttonPdf_Click(object sender, EventArgs e)
+        private void buttonTXT_Click(object sender, EventArgs e)
         {
-
+            option = "TXT";
+            DialogResult = DialogResult.OK;
+            Close();
         }
     }
 }
